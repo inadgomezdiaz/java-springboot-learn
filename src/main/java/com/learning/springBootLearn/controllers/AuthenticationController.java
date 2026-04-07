5a6329ef4543dd947d1bd57d684ffca9de1b4aab
inadgomezdiaz <dngmdz18@gmail.com>
2026-04-07 13:32:11 -0300

    feat: create AuthenticationController

---
+++ src/main/java/com/learning/springBootLearn/controllers/AuthenticationController.java
@@ -0,0 +1,46 @@
+package com.learning.springBootLearn.controllers;
+
+import com.learning.springBootLearn.dto.LoginResponseDto;
+import com.learning.springBootLearn.dto.LoginUserDto;
+import com.learning.springBootLearn.dto.RegisterUserDto;
+import com.learning.springBootLearn.entities.User;
+import com.learning.springBootLearn.services.AuthenticationService;
+import com.learning.springBootLearn.services.JwtService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/auth")
+@RestController
+public class AuthenticationController {
+    private final JwtService jwtService;
+
+    private final AuthenticationService authenticationService;
+
+    public AuthenticationController(JwtService jwtService, AuthenticationService authenticationService) {
+        this.jwtService = jwtService;
+        this.authenticationService = authenticationService;
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<User> register(@RequestBody RegisterUserDto registerUserDto) {
+        User registeredUser = authenticationService.signup(registerUserDto);
+
+        return ResponseEntity.ok(registeredUser);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDto> authenticate(@RequestBody LoginUserDto loginUserDto) {
+        User authenticatedUser = authenticationService.authenticate(loginUserDto);
+
+        String jwtToken = jwtService.generateToken(authenticatedUser);
+
+        LoginResponseDto loginResponse = new LoginResponseDto();
+        loginResponse.setToken(jwtToken);
+        loginResponse.setExpiresIn(jwtService.getExpirationTime());
+
+        return ResponseEntity.ok(loginResponse);
+    }
+}
